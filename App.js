7b2ee6fd7cb92bd3f071398f2be3d9f249cf660f
Dawid Popiołek <dawidpopiolek@protonmail.com>
2023-06-11 16:19:34 +0200

    working fetch demo (get github repo names)

--- App.js
+++ App.js
@@ -2,9 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const fetchData = () => {
+    fetch('https://api.github.com/users/popiolekdawid/repos').then(
+      resp => resp.json() // this returns a promise
+    ).then(repos => {
+      for (const repo of repos) {
+        console.log(repo.name);
+      }
+    }).catch(ex => {
+      console.error(ex);
+    })
+  }
+
+  fetchData();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Fetch</Text>
       <StatusBar style="auto" />
     </View>
   );
